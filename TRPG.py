20d72acaab19bffe85b519b0010c8b7acff8de87
userTOP04 <112961367+userTOP04@users.noreply.github.com>
2023-10-10 17:43:22 +0300

    Add files via upload

--- TRPG.py
+++ TRPG.py
@@ -1,11 +1,22 @@
 import os
+from random import randint
 money = 0
 int(money)
 at = 0
 int(at)
 name = input("Введите имя героя ")
+pl_name = name
+xp_pl = 0
+pl_level = 0
+hp_pl = 100 * pl_level
 
-print("ОСТОРОЖНО ЭТА ИГРА 18+ :) (Но для вас мы поставили цизуру)")
+
+en_name = "Жран Борзый"
+en_level = 0
+hp_en = 100 * en_level
+
+
+
 way_left = False
 way_right = False
 way_center = False
@@ -13,7 +24,7 @@ while way_left == False or way_center == False or way_right == False:
     print(f"Подъезжает {name} к трем дорожкам, на перекрестке камень лежит, а на том камне написано: «Кто вправо поедет - тому убитым быть, кто влево поедет - тому богатым быть, а кто прямо поедет - тому женатым быть")
     print("1 - Поеду-ка я по той дорожке, где убитому быть. Умру в чистом поле, как славный богатырь!")
     print("2 - Ну, поеду теперь, где женатому быть!")
-    print("3 - Ну, поеду теперь в дорожку, где богатому бытыть")
+    print("3 - Ну, поеду теперь в дорожку, где богатому быть")
     way = input(f"Куда поедет {name}? ")
 
     if way == "1":
@@ -21,11 +32,47 @@ while way_left == False or way_center == False or way_right == False:
             os.system('cls')
             print(f"{name} ехал по дороге и вдруг ему вдруг ему на встречу разбойники стоят, они потребовали денег но у тебя их нет, ты решил убить их!")
             if at == 10:
-                print("С помощью оружия лавочника вы убили всех разбойников")
+                while True:
+                    player_attack = randint(10, 20) * pl_level
+                    hp_en -= player_attack
+                    print(f"{pl_name} ударил {en_name} на {player_attack} жизней")
+                    print(f"У {en_name} стало {hp_en} жизней")
+                    input("Для продолжения нажмите ENTER")
+                    enemy_attack = randint(0, 5) * en_level
+                    hp_pl -= enemy_attack
+                    print(f"{en_name} ударил {pl_name} на {enemy_attack} жизней")
+                    print(f"У {pl_name} стало {hp_pl} жизней")
+                    input("Для продолжения нажмите ENTER")
+                    if hp_pl <= 0:
+                        print(f"{pl_name} погиб")
+                        break
+                    elif hp_en <= 0:
+                        print(f"{en_name} погиб, вы победили благодоря хлоршему оружию")
+                        xp_pl += (en_level * 10)
+                        while xp_pl >= 10:
+                            pl_level += 1
+                            xp_pl -= 10
+                        break
             else:
-                print("У вас нет хорошего оружия, вас убили разбойники!")
+                while True:
+                    player_attack = randint(0, 3) * pl_level
+                    hp_en -= player_attack
+                    print(f"{pl_name} ударил {en_name} на {player_attack} жизней")
+                    print(f"У {en_name} стало {hp_en} жизней")
+                    input("Для продолжения нажмите ENTER")
+                    enemy_attack = randint(5, 10) * en_level
+                    hp_pl -= enemy_attack
+                    print(f"{en_name} ударил {pl_name} на {enemy_attack} жизней")
+                    print(f"У {pl_name} стало {hp_pl} жизней")
+                    input("Для продолжения нажмите ENTER")
+                    if hp_pl <= 0:
+                        print(f"{pl_name} погиб из зи отсудствия хорошего оружия")
+                        break
+                    elif hp_en <= 0:
+                        print(f"{en_name} погиб")
+                        break
                 break
-            way_right == True
+            way_right = True
         else:
             print("Вы уже были на этой дороге")
     elif way == "2":
@@ -33,7 +80,7 @@ while way_left == False or way_center == False or way_right == False:
             os.system('cls')
             print(f"{name} ехал по дороге и увидел замок, там в темнице была девушка вы освободили ее!")
             print("Оказалось что эта девкшка была княжной но ее заперли в собственной темнице")
-            dont_agree = input('Девушка предлагает перес@@@@, 1 - Отказать, 2 - Согласиться')
+            dont_agree = input('Девушка предлагает перес@@@@, 1 - Согласился, 2 - Отказать')
             if dont_agree == "1":
                 print(f"Княжна заманила вас в комнату.(НА СЛЕДУЮЩИЕ УТРО) княжна придложила обручиться и {name} согласился, но с каждым днем жизни вы чувствовали себя хуже и хуже пока одним вечером не узнали что княжна травит вас чтобы после вашей смерти получить богатство {name}.Плохая концовка :(")
                 break
