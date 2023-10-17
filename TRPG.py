c16cab02d0ad7c814a08e67dfaf3fb4acda390fc
userTOP04 <112961367+userTOP04@users.noreply.github.com>
2023-10-17 17:48:35 +0300

    Add files via upload

--- TRPG.py
+++ TRPG.py
@@ -1,11 +1,10 @@
 import os
-from random import randint
-money = 10000
+import random
+money = 100
 int(money)
 at = 0
 int(at)
-name = input("Введите имя героя ")
-pl_name = name
+pl_name = input("Введите имя героя ")
 xp_pl = 0
 pl_level = 0
 hp_pl = 100 * pl_level
@@ -25,6 +24,8 @@ while way_left == False or way_center == False or way_right == False:
     print("1 - Поеду-ка я по той дорожке, где убитому быть. Умру в чистом поле, как славный богатырь!")
     print("2 - Ну, поеду теперь, где женатому быть!")
     print("3 - Ну, поеду теперь в дорожку, где богатому быть")
+    print("4 - Поеду-ка я в трактир.")
+    print("5 - Пора мне и в магазин съездить")
     way = input(f"Куда поедет {name}? ")
 
     if way == "1":
@@ -80,9 +81,9 @@ while way_left == False or way_center == False or way_right == False:
             os.system('cls')
             print(f"{name} ехал по дороге и увидел замок, там в темнице была девушка вы освободили ее!")
             print("Оказалось что эта девкшка была княжной но ее заперли в собственной темнице")
-            dont_agree = input('Девушка предлагает перес@@@@, 1 - Согласился, 2 - Отказать')
+            dont_agree = input('Девушка предлагает пойти в комнату, 1 - Согласился, 2 - Отказать')
             if dont_agree == "1":
-                print(f"Княжна заманила вас в комнату.(НА СЛЕДУЮЩИЕ УТРО) княжна придложила обручиться и {name} согласился, но с каждым днем жизни вы чувствовали себя хуже и хуже пока одним вечером не узнали что княжна травит вас чтобы после вашей смерти получить богатство {name}.Плохая концовка :(")
+                print(f"Княжна заманила вас в комнату и убила")
                 break
             elif dont_agree == "2":
                 print(f"Вы сказали кнежне что скоро прийдете, но на самом деле {name} пытается выбраться через туалет и случайно дергает за рычаг открываеться тойнвй проход и {name} по тунелю выбираеться к камню")
@@ -93,37 +94,6 @@ while way_left == False or way_center == False or way_right == False:
                 break
         else:
             print("Вы уже были на этой дороге")
-        print("Вас приветствуют в таверне и предлагают сыграть в кости. 1 - Да отичное предложение. 2 - Нет")
-        while True:
-            bet = int(input("Выбери ставку "))
-            if bet <= 0:
-                print("Нельзя поставить такую ставку")
-                input("Нажмите ENTER для продолжения")
-                break
-            if bet > money:
-                (f"У {pl_name} недстадочно денег для такой ставки")
-                input("Нажмите ENTER для продолжения")
-                break
-            user_turn = random.randint(1, 6)
-            casino_turn = random.randint(1, 6)
-
-            print("Игрок выбросил", user_turn)
-            print("Казино выбросил", casino_turn)
-
-            if casino_turn > user_turn:
-                print("Казино победил")
-                money -= 100
-            elif casino_turn < user_turn:
-                print("Игрок победил")
-                roney_user += 100
-            else:
-                print("Ничья")
-            breakk = int(input(f"{pl_name} хотите выйти. 1 - Да, 2 - Нет"))
-            if breakk == 1:
-                break
-            elif breakk == 2:
-                pass
-
     elif way == "3":
         if way_left == False:
             os.system('cls')
@@ -135,23 +105,56 @@ while way_left == False or way_center == False or way_right == False:
                 break
             elif way_3 == "2":
                 os.system('cls')
-                print(f"{name} поехал дальше и нашел пещеру с богатствами, он забрал их себе и поехал в придорожную лавку")
+                print(f"{name} поехал дальше и нашел пещеру с богатствами, он забрал их себе и поехал к камню")
                 money += 1000
                 way_left = True
-                print(f"В лавке вас ожидал продавец и он спросил вас хотите ли вы купить кибер броню и кибер меч за 1000 монет. У вас {money} денег")
-                if money >= 1000:
-                    print("Поздравляю вы приобрели кибер броню и кибер меч за 1000 монет вы отправляетесь дальше")
-                    at += 10
-                elif money < 1000:
-                    print("У вас нехватило денег и вы поехали дальше")
             else:
                 print("Такого выбора не предусматривалось возможно в будующем это исправят, ХОТЯ МЫ ОБА ЗНАЕМ ЧТО ТЫ ПРОСТО ГРЯЗНЫЙ ХАКЕР")
                 print("YOU DEAD АХАХ ИГРАЙ СНАЧАЛА")
                 break
         else:
             print("Вы уже были на этой дороге")
+    elif way == "4":
+        way123 = int(input("Вас приветствуют в таверне и предлагают сыграть в кости. 1 - Да отичное предложение. 2 - Нет"))
+        if way123 == 1:
+            while True:
+                bet = int(input("Выбери ставку "))
+                if bet <= 0:
+                    print("Нельзя поставить такую ставку")
+                    input("Нажмите ENTER для продолжения")
+                    break
+                if bet > money:
+                    (f"У {pl_name} недстадочно денег для такой ставки")
+                    input("Нажмите ENTER для продолжения")
+                    break
+                user_turn = random.randint(1, 6)
+                casino_turn = random.randint(1, 6)
+
+                print("Игрок выбросил", user_turn)
+                print("Трактирщик выбросил", casino_turn)
+
+                if casino_turn > user_turn:
+                    print("Трактирщик победил")
+                    money -= 100
+                    print(f"У {pl_name} {money} монет")
+                elif casino_turn < user_turn:
+                    print("Игрок победил")
+                    roney_user += 100
+                    print(f"У {pl_name} {money} монет")
+                else:
+                    print("Ничья")
+                    print(f"У {pl_name} {money} монет")
+                breakk = int(input(f"{pl_name} хотите выйти. 1 - Да, 2 - Нет"))
+                if breakk == 1:
+                    break
+                elif breakk == 2:
+                    pass
+    elif way == "5":
+        print(f"Торговец приветствует {pl_name} он хочет предложтьсвои товары")
+        print("1 - Зелье здоровья, стоимость 100 монет, лечит 60 hp")
+        print("2 - Большое зелье здоровья, стоимость 300 монет, лечит полное hp")
+        print("3 - Кибер меч, стоимость 1000 монет, увеличивает силу вашего удара и уменьшает силу удара врага")
     else:
         os.system('cls')
-        print(f"{name} хочет выйти из игры. ЧТО? ГРЯЗНЫЙ ХАКЕР ТЫ ДУМАЕШЬ ЧТО МОЖЕШЬ ТАК ПРОСТО ПОКИНУТЬ ЭТО МЕСТО АХАХАХАХАХАХАХАХАХАХАХАХАХАХАХ")
-        print("ПРАВИЛЬНЫЙ ОТВЕТ НЕТ НЕТ НЕТ НЕТ НЕТ НЕТ НЕТ")
+        print(f"{name} такого выюора нет")
 print("Конец:)")
